ec6a25ba5120c8afece5c431230d63d6c11022de
Alejandro Stabile <alejandrostabile@gmail.com>
2021-08-20 19:44:44 -0300

    Adding database for testing

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,17 +16,9 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Alejandro',
-            'email' => 'alejandrostabile@gmail.com',
-            'password' => Hash::make('12345678'),
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s'),
-        ]);
-
-        DB::table('users')->insert([
             'name' => 'Admin',
-            'email' => 'email@example.com',
-            'password' => Hash::make('p@ssword'),
+            'email' => 'admin@example.com',
+            'password' => Hash::make('12345678'),
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ]);
